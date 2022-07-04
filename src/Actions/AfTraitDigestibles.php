fc586f71c7f4598102f2109f04e1ea2592bb5528
Timo Railo <timo@appzio.com>
2022-07-04 09:36:50 +0300

    fix for digestibles

--- src/Actions/AfTraitDigestibles.php
+++ src/Actions/AfTraitDigestibles.php
@@ -12,13 +12,11 @@ use East\LaravelActivityfeed\Models\ActiveModels\AfNotification;
 use East\LaravelActivityfeed\Models\ActiveModels\AfRule;
 use East\LaravelActivityfeed\Models\ActiveModels\AfTemplate;
 use East\LaravelActivityfeed\Models\Helpers\AfCachingHelper;
-use East\LaravelActivityfeed\Models\Helpers\AfDataHelper;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 /*
@@ -41,20 +39,19 @@ trait AfTraitDigestibles
      * @param AfEvent $event
      * @return void
      */
-    public function handleDigestible(AfEvent $event)
-    {
+    public function handleDigestible(AfEvent $event){
 
         // find other digestibles, the digesting time is determined by the first record
-        $records = AfEvent::where('digested', '=', '0')
-            ->where('digestible', '=', '1')
+        $records = AfEvent::where('digested','=','0')
+            ->where('digestible','=','1')
             ->where('id_rule', '=', $event->id_rule)
-            ->with('afRule', 'afRule.afTemplate', 'afRule.afTemplate.afParent')->get();
+            ->with('afRule','afRule.afTemplate','afRule.afTemplate.afParent')->get();
 
-        foreach ($records as $record) {
+        foreach($records as $record){
             $timing = Carbon::now()->addSeconds($record->afRule->digest_delay)->toDateTimeString();
 
-            if ($record->created < $timing) {
-                $this->digest($event, $records);
+            if($record->created < $timing){
+                $this->digest($event,$records);
                 break;
             }
         }
@@ -69,70 +66,48 @@ trait AfTraitDigestibles
      * @param array $records
      * @return void
      */
-    private function digest(AfEvent $event, Collection $records)
-    {
+    private function digest(AfEvent $event, Collection $records){
         $template = '';
-        $class = AfHelper::getTableClass($event->dbtable);
-        $events = [];
-
-        //$models, Collection $results, $relation
-
-
-        
-
-        foreach ($records as $record) {
-            $events[] = AfEvent::match([$event->'Candidates_X_Technologies'],$records,'try');
-        }
-
-        print_r($events);
 
         $vars = [
             'creator' => $event->creator,
-            'events' => $events            // this is a collection including all records
+            'events' => $records
         ];
-
-        print_r($vars);die();
 
         $template_obj = $event->afRule->afTemplate;
         $parent = AfTemplate::find($template_obj->id_parent);
 
-
-
-
-
-
-        $vars = AfRender::eventObjectReplacement($event, $vars);
-        $template = AfRender::renderTemplate($template_obj, $vars, 'digest-');
-
-        if ($event->afRule->afTemplate->afParent()) {
-            $vars = [];
-            $vars['content'] = $template;
-            $template = AfRender::renderTemplate($parent, $vars);
+        foreach ($records as $record) {
+            $vars = AfRender::eventObjectReplacement($record,$vars);
+            $template .= AfRender::renderTemplate($record->afRule->afTemplate,$vars,'digest-');
         }
 
-        print_r($template);
+        if($event->afRule->afTemplate->afParent()){
+            $vars = [];
+            $vars['content'] = $template;
+            $template = AfRender::renderTemplate($parent,$vars);
+        }
+
+        $event->digest_content = $template;
+        $event->processed = 1;
+        $event->digested = 1;
+        $event->save();
 
         foreach ($records as $record) {
-            if ($record->id == $event->id) {
-                $record->digest_content = $template;
-                $record->digestible = 0;         // this is no longer digestible as the digestion is complete
-                $record->processed = 0;          // not needed, just put here for clarity
-                $record->digested = 1;
-                //$record->save();
+            if($record->id == $event->id){
                 continue;
             }
 
             try {
-                //$record->delete();
-            } catch (\Throwable $exception) {
-                Log::error('DIGEST DELETION PROBLEM: ' . $exception->getMessage());
+                $record->delete();
+            } catch (\Throwable $exception){
+                Log::error('DIGEST DELETION PROBLEM: '.$exception->getMessage());
             }
         }
 
-        echo('done');
-        die();
-
     }
+
+
 
 
 }
